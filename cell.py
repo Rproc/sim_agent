f772f9dbe96a704fee5e9a39a2f729c3fae4a051
Rproc <renan_procopio@live.com>
2020-07-13 17:00:01 -0300

    init

---
+++ cell.py
@@ -0,0 +1,6 @@
+class Cell:
+
+    def __init__(self, grid, consolidationTime, decayStartPoint):
+        self.grid = grid
+        self.consolidationTime = consolidationTime
+        self.decayStartPoint = decayStartPoint
